64b6812dee4758baad16dc6940fd062ebf2e15cf
William Rozier <39777112+Wrozier@users.noreply.github.com>
2020-04-14 21:25:03 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,44 +12,63 @@ function App(){
     axios
     .get("https://corona.lmao.ninja/all")
     .then(res => {
-      console.log(res.data);
+      setLatest(res.data);
 
     })
     .catch(err => {
       console.log(err);
     });
-  }, []);
+}, []);
+
+  const date = new Date(parseInt(latest.update));
+  const lastUpdated = date.toString();
+
   return (
     <div> 
       <CardDeck>
-    <Card bg= "secondary" text={"white"} className="text-center" style={{ margin: "10px"}}>
+        <Card 
+          bg= "secondary" 
+          text={"white"} 
+          className="text-center" 
+          style={{ margin: "10px"}}
+        >
       
       <Card.Body>
-        <Card.Title>Cases</Card.Title>
-        <Card.Text> {latest.cases} </Card.Text>
+          <Card.Title>Cases</Card.Title>
+          <Card.Text> {latest.cases} </Card.Text>
       </Card.Body>
-      <Card.Footer>
-        <small>Last updated 3 mins ago</small>
-      </Card.Footer>
+          <Card.Footer>
+        <small>Last updated  {lastUpdated}</small>
+        </Card.Footer>
     </Card>
-    <Card bg="danger" text={"white"} className="text-center" style={{ margin: "10px"}}>
+    <Card 
+    bg="danger" 
+    text={"white"} 
+    className="text-center" 
+    style={{ margin: "10px"}}
+    >
       
       <Card.Body>
         <Card.Title>Deaths</Card.Title>
-        <Card.Text>{latest.cases}</Card.Text>
+        <Card.Text>{latest.deaths}</Card.Text>
       </Card.Body>
       <Card.Footer>
-        <small className="text-muted">Last updated 3 mins ago</small>
+        <small className="text-muted">Last updated {lastUpdated}</small>
       </Card.Footer>
     </Card>
-    <Card>
+
+    <Card
+    bg="success" 
+    text={"white"} 
+    className="text-center" 
+    style={{ margin:"10px"}}>
       
-      <Card.Body bg="success" text={"white"} className="text-center" style={{ margin: "10px"}}>
+      <Card.Body>
         <Card.Title>Recovered</Card.Title>
-        <Card.Text>{latest.cases}</Card.Text>
+        <Card.Text>{latest.recovered}</Card.Text>
       </Card.Body>
       <Card.Footer>
-        <small className="text-muted">Last updated 3 mins ago</small>
+        <small className="text-muted">Last updated {lastUpdated}</small>
       </Card.Footer>
     </Card>
   </CardDeck>
